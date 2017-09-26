9086d5000ed7d0b8bc7e2dcc90f2b72b12ce3605
Patrick Ayres <briceayres@gmail.com>
2017-09-25 19:36:03 -0500

    FIRST BLOOD

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>React Starter App</h1>
+      </div>
+    );
+  }
+}
